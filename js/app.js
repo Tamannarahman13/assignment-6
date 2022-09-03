e3cfb8c9014fb59c5222cd4bd6f93e7f0798d28d
Tamannarahman <tamannarahmanmridha@gmail.com>
2022-09-03 17:53:35 +0600

    news card added

--- js/app.js
+++ js/app.js
@@ -29,12 +29,44 @@ const loadCategoryDetails = async (category_id) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
     const res = await fetch(url);
     const data = await res.json();
-    displayCategoryDetails(data.data[0])
+    displayCategoryDetails(data.data)
+    console.log(data.data)
 
 }
 
-const displayCategoryDetails = (category) => {
-    console.log(category)
+const displayCategoryDetails = (categorysDetails) => {
+    const catagoryDetails = document.getElementById('category-details-container');
+    catagoryDetails.innerText = ''
+    categorysDetails.forEach(categoryDetails => {
+        const categoryDetailDiv = document.createElement('div');
+        categoryDetailDiv.classList.add('card')
+        categoryDetailDiv.innerHTML = `
+        
+  <img src=${categoryDetails.image_url} class="card-img-top" alt="...">
+  <div class="card-body">
+  <h5 class="card-title">${categoryDetails.title}</h5>
+    <p class="card-text">${categoryDetails.details.slice(0, 100)}</p>
+    <div class = "d-flex">
+    <div class = "d-flex">
+    <img src=${categoryDetails.author.img} class="h-25 w-25 rounded-circle"  alt="...">
+    
+    <p class = mt->${categoryDetails.author.name}</p>
+    </div>
+    <p class = "me-5">${categoryDetails.rating.number}</p>
+    <div>
+    <button>more</button>
+    </div>
+    </div>
+    
+</div>
+        
+        
+        `;
+
+        catagoryDetails.appendChild(categoryDetailDiv)
+    })
+
+    // console.log(category)
 }
 
 categoryLoad()
