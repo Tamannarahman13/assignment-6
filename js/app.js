3b0472f09923af23413debff899d38e1c3c062df
Tamannarahman <tamannarahmanmridha@gmail.com>
2022-09-03 23:53:55 +0600

    modal added

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ const loadCategoryDetails = async (category_id) => {
     const res = await fetch(url);
     const data = await res.json();
     displayCategoryDetails(data.data)
-    console.log(data.data)
+    // console.log(data.data[0])
 
 }
 
@@ -42,19 +42,19 @@ const displayCategoryDetails = (categorysDetails) => {
         categoryDetailDiv.classList.add('card')
         categoryDetailDiv.innerHTML = `
         
-  <img src=${categoryDetails.image_url} class="card-img-top" alt="...">
+  <img src=${categoryDetails.thumbnail_url} class="card-img-top h-50 w-full" alt="...">
   <div class="card-body">
   <h5 class="card-title">${categoryDetails.title}</h5>
-    <p class="card-text">${categoryDetails.details.slice(0, 100)}</p>
+    <p class="card-text">${categoryDetails.details.slice(0, 100) + '...'}</p>
     <div class = "d-flex">
     <div class = "d-flex">
-    <img src=${categoryDetails.author.img} class="h-25 w-25 rounded-circle"  alt="...">
+    <img src=${categoryDetails.author.img} class="h-50 w-25 rounded-circle"  alt="...">
     
-    <p class = mt->${categoryDetails.author.name}</p>
+    <p class = mt->${categoryDetails.author.name ? categoryDetails.author.name : 'not found'}</p>
     </div>
     <p class = "me-5">${categoryDetails.rating.number}</p>
     <div>
-    <button>more</button>
+    <button onclick = "loadNewsDetails('${categoryDetails._id}')" class = "border-0" data-bs-toggle="modal" data-bs-target="#newsDetailModal">more..</button>
     </div>
     </div>
     
@@ -67,6 +67,40 @@ const displayCategoryDetails = (categorysDetails) => {
     })
 
     // console.log(category)
+}
+
+
+const loadNewsDetails = async (_id) => {
+    const url = `https://openapi.programming-hero.com/api/news/${_id}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCategoryDetails(data.data)
+    displayNewsDetail(data.data[0])
+
+}
+
+
+
+
+
+const displayNewsDetail = (news) => {
+    console.log(news)
+
+
+    const newsDetail = document.getElementById('news-detail');
+    newsDetail.innerHTML = `
+    <img  src = "${news.image_url}" class = "w-100 h-100" alt = "...">
+    <div>
+    <img  src = "${news.author.img}" class = "w-25 h-25 rounded-circle " alt = "...">
+    <p class= "d-inline-flex">${news.author.name ? news.author.name : 'not found'}</p>
+    <p>${news.author.published_date}</p>
+    <div>
+    <p class = "mt-5 fs-2">${news.title}</p>
+    <p class = "mt-2">${news.details.slice(0, 600)}</p>
+
+    
+    `
+
 }
 
 categoryLoad()
